384c837e8ef439e914f0c58b7ef22d12e54ebe27
andres@digitalhouse.com <andres@digitalhouse.com>
2018-11-30 19:11:09 -0500

    Mumuki Export on 2018-11-30 19:11:09 -0500

--- 00003_Listas/test.py
+++ 00003_Listas/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
   def test_numeros(self):
-    self.assertEquals(numeros_escritos, ["uno", "dos", "tres"]])
+    self.assertEquals(numeros_escritos, ["uno", "dos", "tres"])
