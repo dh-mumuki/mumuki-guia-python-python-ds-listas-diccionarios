d213949b692f0e6c46af363a1f52fdc30707ea28
andres@digitalhouse.com <andres@digitalhouse.com>
2018-11-30 19:19:39 -0500

    Mumuki Export on 2018-11-30 19:19:39 -0500

---
+++ 00004_Acceder a una lista/default.py
@@ -0,0 +1,9 @@
+varios = [1, 'a', 2, "b", 3, "c", 4, 'd', 5, "e"]
+
+numeros =
+letras =
+vocales =
+pares = 
+primos =
+
+inverso
---
+++ 00004_Acceder a una lista/test.py
@@ -0,0 +1,15 @@
+class TestFixtures(unittest.TestCase):
+  def test_numeros(self):
+    self.assertEquals(numeros, ["uno", "dos", "tres"])
+
+  def test_numeros(self):
+    self.assertEquals(letras, ["uno", "dos", "tres"])
+
+  def test_numeros(self):
+    self.assertEquals(vocales, ["uno", "dos", "tres"])
+
+  def test_numeros(self):
+    self.assertEquals(primos, ["uno", "dos", "tres"])
+
+  def test_numeros(self):
+    self.assertEquals(bonus, ["uno", "dos", "tres"])
