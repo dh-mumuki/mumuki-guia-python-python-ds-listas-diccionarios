86c539d7ce76899314d5d2c8365c96633645a94e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-01 08:30:03 -0500

    Mumuki Export on 2018-12-01 08:30:03 -0500

--- 00008_Operaciones con diccionarios/test.py
+++ 00008_Operaciones con diccionarios/test.py
@@ -1,1 +1,3 @@
-#
+class TestFixtures(unittest.TestCase):
+  def test_listas(self):
+    self.assertEquals(sede_dh, {"barrio": "Vila Olimpia", 'direccion': 'Av. Dr. Cardoso de Melos', 'lugar': "Digital House"})
