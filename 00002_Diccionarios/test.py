9c3e4cf7b135fdcd237d925be550d380de0fdd97
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-01 08:27:15 -0500

    Mumuki Export on 2018-12-01 08:27:15 -0500

--- 00002_Diccionarios/test.py
+++ 00002_Diccionarios/test.py
@@ -1,3 +1,4 @@
 class TestFixtures(unittest.TestCase):
   def test_listas(self):
-    self.assertEquals(yo.keys, ['nombre', 'apellido', 'mail'])
+    self.assertEquals(yo.keys(), ['nombre', 'apellido', 'mail'])
+    
