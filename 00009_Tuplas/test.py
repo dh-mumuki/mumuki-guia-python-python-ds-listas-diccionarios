620e4309e6d0bf8122fc0edce2d797e3ff854fd6
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 15:10:28 -0500

    Mumuki Export on 2018-12-14 15:10:28 -0500

--- 00009_Tuplas/test.py
+++ 00009_Tuplas/test.py
@@ -1,6 +1,9 @@
 class TestFixtures(unittest.TestCase):
-  def test_listas(self):
+  def test_tomate(self):
     self.assertEquals(verduras[1].lower(), "tomate")
 
-  def test_listas(self):
-    self.assertEquals(len(verduras), 5)    
+  def test_tamano(self):
+    self.assertEquals(len(verduras), 5)
+  
+  def test_tupla(self):
+    assertIsInstance(verduras, tuple)
