ba64e4d7bf166d4d6bcad9f7a54fe39d3832cf58
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 15:11:21 -0500

    Mumuki Export on 2018-12-14 15:11:21 -0500

--- 00009_Tuplas/test.py
+++ 00009_Tuplas/test.py
@@ -1,9 +1,9 @@
 class TestFixtures(unittest.TestCase):
   def test_tomate(self):
-    self.assertEquals(verduras[1].lower(), "tomate")
+    self.assertEquals(verduras[1].lower(), "tomate", "El segundo elemento no es tomate")
 
   def test_tamano(self):
-    self.assertEquals(len(verduras), 5)
+    self.assertEquals(len(verduras), 5, "El contenedor no tiene 5 elementos")
   
   def test_tupla(self):
-    assertIsInstance(verduras, tuple)
+    assertIsInstance(verduras, tuple, "El elemento no es una tupla")
