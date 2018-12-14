e7a7b750a28175209945083708824dc05a45f38d
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-14 15:06:50 -0500

    Mumuki Export on 2018-12-14 15:06:50 -0500

--- 00008_Operaciones con diccionarios/test.py
+++ 00008_Operaciones con diccionarios/test.py
@@ -1,12 +1,16 @@
 class TestFixtures(unittest.TestCase):
-  def test_listas(self):
-    self.assertEquals(sede_dh['barrios'].lower(), "vila olimpia")
+  def test_listas_barrio(self):
+    self.assertTrue("vila" in sede_dh['barrios'].lower())
     
-  def test_listas(self):
-    self.assertEquals(sede_dh['direccion'].lower(), "av. dr. cardoso de melo")
+  def test_listas_direccion(self):
+    self.assertTrue("cardoso" in sede_dh['direccion'].lower())
+    self.assertTrue("de" in sede_dh['direccion'].lower())
+    self.assertTrue("melo" in sede_dh['direccion'].lower())
 
-  def test_listas(self):
-    self.assertEquals(sede_dh['lugar'].lower(), "digital house")
+  def test_listas_lugar(self):
+    self.assertTrue("digital" in sede_dh['lugar'].lower())
+    self.assertTrue("house" in sede_dh['lugar'].lower())
+    
 
-  def test_listas(self):
-    self.assertEquals(sede_dh['pais'].lower(), "brasil")
+  def test_listas_pais(self):
+    self.assertTrue("brasil" in sede_dh['pais'].lower())
