210b84abc7d3b04eb2b28d46abac5058a7014e27
andres@digitalhouse.com <andres@digitalhouse.com>
2018-12-20 15:36:14 -0500

    Mumuki Export on 2018-12-20 15:36:14 -0500

--- 00006_Método append()/test.py
+++ 00006_Método append()/test.py
@@ -1,6 +1,3 @@
 class TestFixtures(unittest.TestCase):
   def test_listas(self):
-    try:
-      self.assertEqual(lista, ['python', 43, 45.24, False, [2, 4, 5]], msg = 'Revisá si cumple con todas la condiciones!')
-    except AssertionError as e:
-      print(e)
+      self.assertEqual(lista, ['python', 43, 45.24, False, [2, 4, 5]], msg = 'Revisá si cumple con todas las condiciones!')
