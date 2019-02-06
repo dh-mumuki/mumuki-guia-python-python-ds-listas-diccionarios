4421863bebee70e40241742e0f688cb2599e32a1
Andres Furlan <Andres Furlan>
2019-02-06 23:24:39 +0000

    Mumuki Export on 2019-02-06 23:24:39 +0000

--- 00008_Actualizando diccionarios/test.py
+++ 00008_Actualizando diccionarios/test.py
@@ -1,16 +1,16 @@
 class TestFixtures(unittest.TestCase):
   def test_listas_barrio(self):
-    self.assertTrue("vila" in sede_dh['barrio'].lower())
+    self.assertTrue("vila" in sede_dh['barrio'].lower(), 'Barrio no especifica vila')
     
   def test_listas_direccion(self):
-    self.assertTrue("cardoso" in sede_dh['direccion'].lower())
-    self.assertTrue("de" in sede_dh['direccion'].lower())
-    self.assertTrue("melo" in sede_dh['direccion'].lower())
+    self.assertTrue("cardoso" in sede_dh['direccion'].lower(), 'Cardoso de melo no se encuentra en direccion')
+    self.assertTrue("de" in sede_dh['direccion'].lower(), 'Cardoso de melo no se encuentra en direccion')
+    self.assertTrue("melo" in sede_dh['direccion'].lower(), 'Cardoso de melo no se encuentra en direccion')
 
   def test_listas_lugar(self):
-    self.assertTrue("digital" in sede_dh['lugar'].lower())
-    self.assertTrue("house" in sede_dh['lugar'].lower())
+    self.assertTrue("digital" in sede_dh['lugar'].lower(), 'digital house no se encuentra en lugar')
+    self.assertTrue("house" in sede_dh['lugar'].lower(),'digital house no se encuentra en lugar')
     
 
   def test_listas_pais(self):
-    self.assertTrue("brasil" in sede_dh['pais'].lower())
+    self.assertTrue("brasil" in sede_dh['pais'].lower(), 'Brasil no se encuentra en pais')
