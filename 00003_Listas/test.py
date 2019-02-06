7e56d1077923b00de76574573e2fd3755e25e62e
Andres Furlan <Andres Furlan>
2019-02-06 23:10:04 +0000

    Mumuki Export on 2019-02-06 23:10:04 +0000

--- 00003_Listas/test.py
+++ 00003_Listas/test.py
@@ -1,3 +1,4 @@
 class TestFixtures(unittest.TestCase):
-  def test_numeros(self):
-    self.assertEquals(numeros_escritos, ["uno", "dos", "tres"])
+  def test_lista_contenido(self):
+    self.assertTrue(isinstance(numeros_escritos, (list)), 'el valor numeros_escritos no es una lista')
+    self.assertEquals(numeros_escritos, ["uno", "dos", "tres"], 'La lista no tiene los elementos requeridos')
