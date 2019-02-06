e085a2114a0c32efb6eda022bd49a211deb5263a
Andres Furlan <Andres Furlan>
2019-02-06 23:20:34 +0000

    Mumuki Export on 2019-02-06 23:20:34 +0000

--- 00002_Diccionarios/test.py
+++ 00002_Diccionarios/test.py
@@ -1,5 +1,5 @@
 class TestFixtures(unittest.TestCase):
   def test_keys(self):
-    self.assertEquals(yo.keys(), ['nombre', 'apellido', 'mail'])
+    self.assertEquals(yo.keys(), ['nombre', 'apellido', 'mail'], 'Faltan claves en el diccionario.')
   def test_values(self):
-    self.assertAlmostEqual(len([yo[x] for x in yo if yo[x]!=None]), 3)
+    self.assertAlmostEqual(len([yo[x] for x in yo if yo[x]!=None]), 3, 'Faltan valores para alguna/s clave/s')
