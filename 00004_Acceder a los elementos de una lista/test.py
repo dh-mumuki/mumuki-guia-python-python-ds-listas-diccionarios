b57b6609616edd582cf9ed16bad485e7f295b6c6
andres@digitalhouse.com <andres@digitalhouse.com>
2019-06-10 14:27:20 +0000

    Mumuki Export on 2019-06-10 14:27:20 +0000

--- 00004_Acceder a los elementos de una lista/test.py
+++ 00004_Acceder a los elementos de una lista/test.py
@@ -12,7 +12,7 @@ class TestFixtures(unittest.TestCase):
     self.assertEquals(pares, [2, 4], 'El contenido de la lista pares, no es correcto')
 
   def test_primos(self):
-    self.assertEquals(primos, [1, 3, 5], 'El contenido de la lista primos, deben ser numeros primos.')
+    self.assertEquals(primos, [2, 3, 5], 'El contenido de la lista primos, deben ser numeros primos.')
 
   def test_inversa(self):
     self.assertEquals(inverso,  ['e', 5, 'd', 4, 'c', 3, 'b', 2, 'a', 1], 'El contenido de la lista inverso, debe ser el mismo contenido de la lista original pero empezando por el final hasta el comienzo.')
